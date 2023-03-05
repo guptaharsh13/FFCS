c7f09f0ab9d23d774085f980614e71a4f3b6b868
Harsh Gupta <hg242322@gmail.com>
2023-03-05 09:31:24 +0530

    update: db config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { ClashedSlot } from "../entities/ClashedSlot";
 import { Course } from "../entities/Course";
 import { Faculty } from "../entities/Faculty";
@@ -10,7 +11,19 @@ import { Student } from "../entities/Student";
 import { Timing } from "../entities/Timing";
 import { User } from "../entities/User";
 
-const AppDataSource = new DataSource({
+interface dbConfig {
+  type: string;
+  host: string | undefined;
+  port: number;
+  username: string | undefined;
+  password: string | undefined;
+  database: string | undefined;
+  entities: any[];
+  synchronize: boolean;
+  ssl?: boolean;
+}
+
+const config: dbConfig = {
   type: "postgres",
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
@@ -19,6 +32,12 @@ const AppDataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   entities: [ClashedSlot, Course, Faculty, Slot, Student, Timing, User],
   synchronize: true,
-});
+};
+
+if (Boolean(process.env.REQUIRE_SSL) === true) {
+  config.ssl = true;
+}
+
+const AppDataSource = new DataSource(config as DataSourceOptions);
 
 export default AppDataSource;
